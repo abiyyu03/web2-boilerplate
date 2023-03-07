a698884f0e39faf8382ecee6040d65709ba17588
abiyyu03 <abiyyu652@gmail.com>
2023-03-07 22:33:02 +0700

    tambah boilerplate nilai_mahasiswa

---
+++ pertemuan03/nilai_mahasiswa.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Nilai Mahasiswa</title>
+  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css"> 
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+</head>
+<body>
+    <div class="container">
+      <div class="mt-4 d-flex justify-content-between">
+        <h3>Daftar Nilai Mahasiswa</h3>
+        <a href="form_nilai.php" class="btn btn-primary"><i class="fa fa-plus"></i> Tambah Data</a>
+      </div>
+      <div class="table-responsive mt-3">
+        <table class="table table-striped">
+          <thead>
+            <tr>
+              <th>#</th>
+              <th>Nama Mahasiswa</th>
+              <th>Mata Kuliah</th>
+              <th>Nilai UTS</th>
+              <th>Nilai UAS</th>
+              <th>Nilai Tugas</th>
+              <th>Rata-rata</th>
+              <th>Grade</th>
+            </tr>
+          </thead>
+          <tbody>
+
+          </tbody>
+        </table>
+      </div>
+    </div>
+</body>
+</html>
